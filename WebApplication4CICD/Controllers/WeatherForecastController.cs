ec17cd70784853b79c01dd91b71eb524f3cdc274
Tanveer <tanveer.singh.pahwa@gmail.com>
2026-05-23 13:19:38 +0530

    Add initial code
    Add initial code

---
+++ WebApplication4CICD/Controllers/WeatherForecastController.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication4CICD.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet]
+        [Route("GetTestMethod")]
+        public async Task<IActionResult> GetTestMethod()
+        {
+            var response = new
+            {
+                Product = new[]
+                {
+                    "Pre-Claim_TestProduct1",
+                    "Pre-Claim_TestProduct2"
+                }
+            };
+
+            return Ok(response);
+        }
+    }
+}
